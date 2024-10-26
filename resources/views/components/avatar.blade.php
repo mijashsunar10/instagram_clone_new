677f36a4d786d326050f9a3b68e13493ff3d31c3
mijashsunar10 <mijashsunar.xdezo@gmail.com>
2024-10-26 12:55:40 +0545

    Completed the story section of the instagram

--- resources/views/components/avatar.blade.php
+++ resources/views/components/avatar.blade.php
@@ -13,6 +13,8 @@
         />
     @endif
 
+   
+
     @if (!$src)
         <svg
             class="shrink-0 w-full h-full rounded-full text-gray-300 bg-gray-100 dark:bg-gray-600"
@@ -20,6 +22,7 @@
             viewBox="0 0 24 24">
             <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
         </svg>
+         {{-- Here if the image source is not available then the below image souorce will come automatically  --}}
     @endif
 </div>
 
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -11,6 +11,40 @@
     <main class="grid lg:grid-cols-12 gap-8 md:mt-10 ">
         <aside class="lg:col-span-8 border overflow-hidden h-[1000px]" >
 
+            {{-- Stories --}}
+            <section>
+
+                <ul class="flex overflow-x-auto items-center gap-2">
+                    
+                        {{-- Heere if image source is not available then avatar source will automativally come here --}}
+                        {{-- <x-avatar src="https://source.unsplash.com/500x500?face" class="h-14 w-14"/>  --}}
+                        {{-- <x-avatar story src="https://picsum.photos/500" class="h-14 w-14"/>  --}}
+                        {{-- <x-avatar src="https://source.unsplash.com/random/500x500/?face,portrait" class="h-14 w-14" /> --}}
+
+                        @for($i=0;$i<10;$i++)
+                        <li class="flex flex-col justify-center w-20 gap-1 p-2">
+
+                           
+                                <x-avatar story src="https://picsum.photos/500?random={{ rand(1, 100000) }}" class="h-14 w-14"/> 
+
+                               
+                                <p class="text-xs font-medium truncate">
+                                    {{fake()->name}}
+    
+                            </p>
+                        </li>
+                        @endfor
+                       
+
+                       
+
+
+                    
+
+                </ul>
+
+            </section>
+
         </aside>
 
         <aside class="lg:col-span-4 border hidden lg:block p-4">
