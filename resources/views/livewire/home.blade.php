843ab808d9ab6f1b3ae8da9d7ded258a2fe9f7cc
mijashsunar10 <mijashsunar.xdezo@gmail.com>
2024-10-26 08:22:40 +0545

    Created first livewire component Home

---
+++ resources/views/livewire/home.blade.php
@@ -0,0 +1,20 @@
+<div class="w-full">
+    
+    {{-- header --}}
+
+    <header>
+        
+    </header>
+
+    {{-- main --}}
+
+    <main class="grid lg:grid-cols-12 gap-8 md:mt-10 ">
+        <aside class="lg:col-span-8 border overflow-hidden h-[1000px]" >
+
+        </aside>
+
+        <aside class="lg:col-span-4 border hidden lg:block p-4">
+            
+        </aside>
+    </main>
+</div>
