33ae68f9ccb609eaad0088e3f01b1086f3564db2
mijashsunar10 <mijashsunar.xdezo@gmail.com>
2024-10-28 09:19:41 +0545

    Designed mobile header

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -2,7 +2,35 @@
     
     {{-- header --}}
 
-    <header>
+    <header class="md:hidden sticky top-0 bg-white">
+        <div class="grid grid-cols-12 gap-2 items-center">
+
+            <div class="col-span-3">
+
+                <img src="{{asset('assets/logo.png')}}" alt="logo" class="h-12 max-w-lg w-full">
+
+            </div>
+            <div class="col-span-8 flex justify-center px-2">
+                <input type="text" placeholder="Search" class="border-0 outline-none w-full focus:outline-none bg-gray-100 rounded-lg focus:ring-0 hover:ring-0">
+
+
+            </div>
+            <div class="col-span-1 flex justify-center">
+                <a href="#">
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.9" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                          </svg>
+                          
+
+                    </span>
+                </a>
+
+
+            </div>
+        </div>
+
+
         
     </header>
 
@@ -60,7 +88,7 @@
                         </h4>
                     </div>
                     <div class="col-span-2 flex text-right justify-end">
-                        <button class="font-bold text-blue-500 ml-auto text-sm">Follow</button>
+                        <button class="font-bold text-blue-500 ml-auto text-sm">See All</button>
                     </div>
                 </div>
 
@@ -92,7 +120,7 @@
 
             {{-- App links --}}
 
-            <ul class="flex gap-5 flex-wrap list-disc mt-5 m-3">
+            <ul class="flex gap-3 flex-wrap  mt-5 ">
                 <li class="text-xs text-gray-400 font-medium ">
                 <a href="#" class="hover:underline">About</a>
                 </li>
@@ -125,7 +153,7 @@
                 </li>
             </ul>
 
-            <p class="text-sm mt-3  text-gray-400">&#169; 2024 INSTAGRAM FROM META</p>
+            <p class="text-sm mt-4  text-gray-400">&#169; 2024 INSTAGRAM FROM META</p>
             
         </aside>
     </main>
