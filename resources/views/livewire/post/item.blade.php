d517a86b1b84fd14a510ffeafb9e229da79cacc4
mijashsunar10 <mijashsunar.xdezo@gmail.com>
2024-11-01 18:47:39 +0545

    Added number of likes and description and show the comments

--- resources/views/livewire/post/item.blade.php
+++ resources/views/livewire/post/item.blade.php
@@ -56,7 +56,7 @@
             </span>
 
             <span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send w-5 h-5" viewBox="0 0 16 16">
                     <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"/>
                   </svg>
 
@@ -68,11 +68,25 @@
                   </svg>
                   
             </span>
+        </div>
+
+        {{-- likes and views --}}
+
+        <p class="font-bold text-sm">
+            104,474 likes
+        </p>
+
+        {{-- name and comment --}}
+        <div class="text-sm gap-2 font-medium">
+            <p><strong class="font-bold">{{fake()->name}}</strong>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, molestiae.</p>
+        </div>
+
+        {{-- view post modal --}}
+        <button class="text-slate-500/90 text-sm font-medium">View all 456 comments</button>
 
         
         
-            
-        </div>
     </footer>
 
 
