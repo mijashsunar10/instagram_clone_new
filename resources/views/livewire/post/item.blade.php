9d70ca99c2a501730ac0bc6187a471100af6449f
mijashsunar10 <mijashsunar.xdezo@gmail.com>
2024-11-01 20:19:38 +0545

    Added the alphine js for the comment, post button will be seen only written something in comment

--- resources/views/livewire/post/item.blade.php
+++ resources/views/livewire/post/item.blade.php
@@ -89,14 +89,19 @@
 
         <form x-data="{inputText:''}" action="" class="grid grid-cols-12 items-center w-full">
             @csrf
-            <input type="text" placeholder="Leave a comment" class="border-0 col-span-10 placeholder:text-sm outline-none focus:outline-none px-0 rounded-lg hover:ring-0 focus:ring-0">
+            <input x-model="inputText" type="text" placeholder="Leave a comment" class="border-0 col-span-10 placeholder:text-sm outline-none focus:outline-none px-0 rounded-lg hover:ring-0 focus:ring-0">
             <div class="col-span-1 ml-auto flex justify-end text-right">
-                <button class="text-sm font-semibold flex justify-end text-blue-500">
+                <button x-cloak x-show="inputText.length >0" class="text-sm font-semibold flex justify-end text-blue-500">
                     Post
                 </button>
             </div>
 
-            <span class="col-span-1 ml-auto"></span>
+            <span class="col-span-1 ml-auto">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z" />
+                  </svg>
+                  
+            </span>
         </form>
         
         
