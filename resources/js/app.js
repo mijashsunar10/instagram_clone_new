a9acf7b7cb884e073614c085bab9861044bc38d4
mijashsunar10 <mijashsunar.xdezo@gmail.com>
2024-11-01 21:38:01 +0545

    Made the navigation botton same as instagram

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,7 +20,7 @@ import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
 // import Swiper and modules styles
 import 'swiper/css';
-import 'swiper/css/navigation';
+// import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 
--- resources/views/livewire/post/item.blade.php
+++ resources/views/livewire/post/item.blade.php
@@ -55,6 +55,7 @@
                     <!-- Slides -->
                     <div class="swiper-slide"><x-video source="https://cdn.devdojo.com/pines/videos/coast.mp4" /></div>
                     <div class="swiper-slide"><img src="https://images.pexels.com/photos/28050583/pexels-photo-28050583/free-photo-of-kapadokya.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" class="h-[500px] w-full block object-scale-down"></div>
+                    <div class="swiper-slide"><img src="https://images.pexels.com/photos/28050583/pexels-photo-28050583/free-photo-of-kapadokya.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" class="h-[500px] w-full block object-scale-down"></div>
                     <div class="swiper-slide"><x-video source="https://cdn.devdojo.com/pines/videos/coast.mp4" /></div>
                     <!-- Add more slides as needed -->
                 </div>
@@ -63,8 +64,28 @@
                 <div class="swiper-pagination"></div>
     
                 <!-- Navigation buttons -->
-                <div class="swiper-button-prev"></div>
-                <div class="swiper-button-next"></div>
+
+                {{-- previous --}}
+                <div class="swiper-button-prev absolute top-1/2 z-10 p-2">
+                    <div class="bg-white/95 border p-1 rounded-full text-gray-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.8" stroke="currentColor" class="w-4 h-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                          </svg>
+                          
+                    </div>
+                </div>
+
+                {{-- next --}}
+                <div class="swiper-button-next absolute right-0 top-1/2 z-10 p-2">
+
+                    <div class="bg-white/95 border p-1 rounded-full text-gray-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.8" stroke="currentColor" class="size-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                          </svg>
+                          
+                          
+                    </div>
+                </div>
     
                 <!-- Scrollbar (optional) -->
                 <div class="swiper-scrollbar"></div>
